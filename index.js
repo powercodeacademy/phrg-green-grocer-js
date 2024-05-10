0f0f3e72118dde62ebf7607ac3bc05be7b4037da
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-10 14:09:33 -0400

    Initial commit before swithch

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+const cartArray = [
+  {"AVOCADO": {price: 3.0, clearance: true}},
+  {"AVOCADO": {price: 3.0, clearance: true}},
+  {"KALE": {price: 3.0, clearance: false}}
+]
+function CB(cartItem) {
+  let food = Object.keys(cartItem)
+    return food
+}
+
 const consolidateCart = (cart) => {
   // code here
+  let something = cart.map(CB (0))
+
+  // consolidater function
+  cartObj = Object.assign({}, cart)
+  
+  console.log(cartObj)
+  //receives array and returns and obj - converstion
+  // count of individual food in cart and add to obj
+  // if item is present it logs and 
+
 }
 
 const applyCoupons = (cart, coupons) => {
@@ -13,3 +33,6 @@ const applyClearance = (cart) =>{
 const checkout = (cart, coupons) => {
   // code here
 }
+
+// consolidateCart(cartArray)
+console.log(CB({"AVOCADO": {price: 3.0, clearance: true}}))
