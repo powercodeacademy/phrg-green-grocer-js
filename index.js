e5e1eec9b278933acb601f3b4ae948365ff77804
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-10 15:24:59 -0400

    Loop through array and make new object with no dups

--- index.js
+++ index.js
@@ -3,24 +3,34 @@ const cartArray = [
   {"AVOCADO": {price: 3.0, clearance: true}},
   {"KALE": {price: 3.0, clearance: false}}
 ]
-function CB(cartItem) {
-  let food = Object.keys(cartItem)
-    return food
-}
+//
 
 const consolidateCart = (cart) => {
   // code here
-  let something = cart.map(CB (0))
-
-  // consolidater function
-  cartObj = Object.assign({}, cart)
+  // create a new object, 
+  let newCart = {}
+  //check each item is in the cart
+  let countValue = 1
+  cart.forEach(item => {
+    //if item is in cart, count++ 
+    if (item in newCart){
+      countValue++
+    } else {
+      //else if not in the cart, add it to the cart
+      newCart[Object.keys(item)[0]] = Object.values(item)[0] 
+    }
+    debugger
+  })
+  console.log(newCart)
+   debugger
+  //else if not in the cart, add it to the cart
+  //cartObj = Object.assign({}, cart)
   
-  console.log(cartObj)
   //receives array and returns and obj - converstion
   // count of individual food in cart and add to obj
-  // if item is present it logs and 
 
 }
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 const applyCoupons = (cart, coupons) => {
   // code here
@@ -34,5 +44,5 @@ const checkout = (cart, coupons) => {
   // code here
 }
 
-// consolidateCart(cartArray)
-console.log(CB({"AVOCADO": {price: 3.0, clearance: true}}))
+consolidateCart(cartArray)
+
