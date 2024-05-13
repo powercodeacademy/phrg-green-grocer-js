bf395da86b1e2dbf5bb39e910f94083c8263c984
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-13 11:32:56 -0400

    Removed code comments for consolidate cart

--- index.js
+++ index.js
@@ -2,42 +2,31 @@ const cartArray = [
   {"AVOCADO": {price: 3.0, clearance: true}},
   {"AVOCADO": {price: 3.0, clearance: true}},
   {"KALE": {price: 3.0, clearance: false}},
-  {"KALE": {price: 3.0, clearance: false}}
+ 
 
 ]
-//
 
 const consolidateCart = (cart) => {
-  // code here
-  // create a new object, 
+  
   let newCart = {}
-  //check each item is in the cart
+  
   
   cart.forEach(item => {
-    //if item is in cart, count++ 
+    
     const itemName = Object.keys(item)[0]
     if (itemName in newCart){
       newCart[itemName].count += 1
     } else {
-      //else if not in the cart, add it to the cart
-      //get item values
+      
       let itemValues = Object.values(item)[0] 
-      //add count 1 to values
+      
       itemValues["count"] = 1
-            //assign values to object
+            
       newCart[Object.keys(item)[0]] = itemValues
     }
-    
   })
   return newCart
-  
-  
-  //else if not in the cart, add it to the cart
-  //cartObj = Object.assign({}, cart)
-  
-  //receives array and returns and obj - converstion
-  // count of individual food in cart and add to obj
-
+ 
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
