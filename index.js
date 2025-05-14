80b65ceb620edb5688121d3641fb7c5d9227e521
Kyle Housel <kylegranthousel@gmail.com>
2025-05-14 06:13:39 -0400

    Levelheaded attempt up to applyClearance

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 let items = [
   { AVOCADO: { price: 3.0, clearance: true } },
-  { AVOCADO: { price: 3.0, clearance: true } },
-  { AVOCADO: { price: 3.0, clearance: true } },
   { KALE: { price: 3.0, clearance: false } },
   { BLACK_BEANS: { price: 2.5, clearance: false } },
-  { CHEESE: { price: 6.5, clearance: false } },
   { ALMONDS: { price: 9.0, clearance: false } },
-  { TEMPEH: { price: 3.0, clearance: true } },
-  { TEMPEH: { price: 3.0, clearance: true } },
-  { TEMPEH: { price: 3.0, clearance: true } },
   { TEMPEH: { price: 3.0, clearance: true } },
   { CHEESE: { price: 6.5, clearance: false } },
   { BEER: { price: 13.0, clearance: false } },
@@ -17,88 +11,63 @@ let items = [
   { "SOY MILK": { price: 4.5, clearance: true } },
 ]
 
-// // Expected output:
-// // {
-// //   "AVOCADO": {price: 3.0, clearance: true, count: 2},
-// //   "KALE": {price: 3.0, clearance: false, count: 1}
-// // }
+const consoCart = [
+  {"AVOCADO": {price: 3.0, clearance: true}},
+  {"AVOCADO": {price: 3.0, clearance: true}},
+  {"KALE": {price: 3.0, clearance: false}}
+]
 
-//   //Declare empty object
-//   //Loop through cart, push key, value pair into object
-//   //If key exists, increment count
-// const consolidatedItems = (cart) => {
-//   const consolidated = {}
+const couponz = [
+  { item: "AVOCADO", num: 2, cost: 5.0 },
+  { item: "BEER", num: 2, cost: 20.0 },
+  { item: "CHEESE", num: 3, cost: 15.0 },
+]
 
-//   for (let item of cart){
-//     const[key, value] = Object.entries(item)[0]
-//     consolidated[key.toString()] = value
-//   }
-//   return consolidated
-// }
-
-// const consolidateCart = (cart) => {
-//   const consolidated = consolidatedItems(cart)
-
-//   for (let key in consolidated) {
-//     consolidated[key].count = 1;
-//   }
-
-//   return consolidated
-// }
 const consolidateCart = (cart) => {
-
   const newCart = {}
 
-
-
-  for (const item of cart) {
-
+  cart.forEach(item =>{
     const itemName = Object.keys(item)[0]
-
+    const itemInfo = item[itemName]
     if (newCart[itemName]) {
-
-    newCart[itemName].count++
-
+      newCart[itemName].count += 1
     } else {
-
-    newCart[itemName] = { ...Object.values(item)[0], count: 1 }
-
+      newCart[itemName] = { ...itemInfo, count: 1 }
     }
-
-  }
-
+  })
   return newCart
-
 }
-const coupons = {item: "AVOCADO", num: 2, cost: 5.0}
 
 const applyCoupons = (cart, coupons) => {
   const updatedCart = {...cart};
 
   coupons.forEach(coupon => {
-    const couponKey = `${coupon.item} W/COUPON`;
-    debugger
-    updatedCart[couponKey] = { ... };
-  });
+    const couponCost = coupon.cost
+    const itemName = coupon.item
+    const discountedItems = coupon.num
+    const itemLabel = `${itemName} W/COUPON`
+    const item = updatedCart[itemName]
+
+
+    if (item && item.count >= discountedItems) {
+      const clearanceStatus = item.clearance;
+
+      if (updatedCart[itemLabel]) {
+        updatedCart[itemLabel].count += 1;
+      } else {
+        updatedCart[itemLabel] = {
+          price: couponCost,
+          count: 1,
+          clearance: clearanceStatus
+        };
+      }
+
+      item.count -= discountedItems;
+    }
+  })
   return updatedCart
-
-  // for (let itemName in cart){
-  //   if (itemName === coupon.tem){
-  //   const couponItemName = `${itemName} W/COUPON`;
-  //   updatedCart[couponItemName] = {};
-  // }
-  // }
-  // return updatedCart;
-
-  // adds a new key, value pair to the cart hash called 'ITEM NAME W/COUPON'"
-// const couponCart = {};
-
-// const itemName = "AVOCADO";
-// const key = `${itemName} W/COUPON`;
-// cart[key] = { num: 2, clearance: true, cost: 5.0 };
-
-// console.log(cart);
 }
+
 const applyClearance = (cart) =>{
   // code here
 }
@@ -107,15 +76,6 @@ const checkout = (cart, coupons) => {
   // code here
 }
 
-
-const cartItems = items.forEach((item) => {
-  item.forEach(e => console.log(e))
-})
-
-// let coupons = [
-//   { item: "AVOCADO", num: 2, cost: 5.0 },
-//   { item: "BEER", num: 2, cost: 20.0 },
-//   { item: "CHEESE", num: 3, cost: 15.0 },
-// ]
-
-// console.log(applyCoupons(consolidateCart(items), coupons))
+// console.log(consolidateCart(consoCart))
+const freshCart = consolidateCart(items)
+console.log(applyCoupons(freshCart, couponz))
